8d6186d4ecd618dbd11591f5b52416b3cc8c31c6
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-07-16 19:10:38 +0800

    项目步骤复盘，总结完成与未完成功能及所遇到的问题

--- flink-sql/MySQL.sql
+++ flink-sql/MySQL.sql
@@ -36,13 +36,14 @@ INSERT INTO area_thresholds (area_id, comfort_threshold, warning_threshold, aler
 ('变形金刚基地', 180, 220, 250); 
 
 -- 创建预警日志表
-CREATE TABLE alert_log (
-    alert_time DATETIME(3) NOT NULL,
-    area_id VARCHAR(255) NOT NULL,
-    current_visitors BIGINT,
-    alert_level VARCHAR(50), -- 例如: '拥挤预警', '警戒预警'
-    alert_message VARCHAR(512)
-);
+-- 我怎么记得这个根本没用到
+-- CREATE TABLE alert_log (
+--     alert_time DATETIME(3) NOT NULL,
+--     area_id VARCHAR(255) NOT NULL,
+--     current_visitors BIGINT,
+--     alert_level VARCHAR(50), -- 例如: '拥挤预警', '警戒预警'
+--     alert_message VARCHAR(512)
+-- );
 
 -- 创建一个名为 area_current_alert 的新表，它代表**“每个区域当前的最新报警状态”**。这张表有主键，可以被 Flink 更新。
 CREATE TABLE area_current_alert (
@@ -51,4 +52,22 @@ CREATE TABLE area_current_alert (
     current_visitors BIGINT,
     alert_level VARCHAR(50),
     alert_message VARCHAR(512)
+);
+
+-- 存储每个游客在每个区域的逗留时长记录
+CREATE TABLE visitor_stay_duration (
+    user_id VARCHAR(255),
+    area_id VARCHAR(255),
+    entry_time DATETIME(3),   -- 进入时间
+    exit_time DATETIME(3),    -- 离开时间
+    duration_minutes BIGINT    -- 逗留时长（分钟）
+);
+
+-- 存储所有被识别出的游客移动路径
+CREATE TABLE visitor_route_log (
+    user_id VARCHAR(255),
+    from_area VARCHAR(255),  
+    to_area VARCHAR(255),    
+    route_time DATETIME(3),  
+    travel_time_minutes BIGINT 
 );
